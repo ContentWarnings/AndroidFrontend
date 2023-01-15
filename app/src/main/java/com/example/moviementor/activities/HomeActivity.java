6a6054f1f6396345ec45b685932b87ca43202e1a
gns12 <garrettspears12@gmail.com>
2023-01-14 19:00:07 -0500

    Created Basic File Directory for Source Code and Renamed MainActivity to HomeActivity

--- app/src/main/java/com/example/moviementor/activities/HomeActivity.java
+++ app/src/main/java/com/example/moviementor/activities/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.example.moviementor;
+package com.example.moviementor.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.moviementor.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_activity);
     }
 }
