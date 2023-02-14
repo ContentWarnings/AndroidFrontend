e67b9d31cf71aa19b5701a21e79eb893354c12c9
gns12 <garrettspears12@gmail.com>
2023-02-14 16:58:46 -0500

    Link search page to search tab button in navigation footer.

--- app/src/main/java/com/example/moviementor/activities/MainActivity.java
+++ app/src/main/java/com/example/moviementor/activities/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.moviementor.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +11,13 @@ import android.widget.LinearLayout;
 
 import com.example.moviementor.R;
 import com.example.moviementor.fragments.FeaturedFragment;
+import com.example.moviementor.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG_FEATURED_FRAGMENT = "FEATURED_FRAGMENT";
+    private final static String TAG_SEARCH_FRAGMENT = "SEARCH_FRAGMENT";
+    private final static String TAG_SETTINGS_FRAGMENT = "SETTINGS_FRAGMENT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +28,10 @@ public class MainActivity extends AppCompatActivity {
         // Fragment is only loaded into container when savedInstanceState is null. This ensures that
         // the home page fragment is added only once: when the main app activity is first created
         if (savedInstanceState == null) {
-            loadFragment(FeaturedFragment.class);
+            final Fragment fragment = new FeaturedFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, fragment, TAG_FEATURED_FRAGMENT).commit();
         }
-    }
-
-    private void loadFragment(final Class fragmentClass) {
-        getSupportFragmentManager().beginTransaction().setReorderingAllowed(true)
-                .replace(R.id.fragment_container, fragmentClass, null).commit();
     }
 
     private void setupNavigationFooter() {
@@ -35,13 +40,58 @@ public class MainActivity extends AppCompatActivity {
         final LinearLayout searchTab = findViewById(R.id.search_tab);
         final LinearLayout settingsTab = findViewById(R.id.settings_tab);
 
-        featuredTab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //loadFragment(FeaturedFragment.class);
+        featuredTab.setOnClickListener(view -> {
+            @Nullable Fragment featuredFragment = getSupportFragmentManager()
+                    .findFragmentByTag(TAG_FEATURED_FRAGMENT);
+
+            if (featuredFragment == null) {
+                featuredFragment = new FeaturedFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragment_container, featuredFragment, TAG_FEATURED_FRAGMENT)
+                        .commit();
+            }
+            else {
+                getSupportFragmentManager().beginTransaction().show(featuredFragment).commit();
+            }
+
+            final @Nullable Fragment searchFragment = getSupportFragmentManager()
+                    .findFragmentByTag(TAG_SEARCH_FRAGMENT);
+            final @Nullable Fragment settingsFragment = getSupportFragmentManager()
+                    .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
+
+            if (searchFragment != null) {
+                getSupportFragmentManager().beginTransaction().hide(searchFragment).commit();
+            }
+            if (settingsFragment != null) {
+                getSupportFragmentManager().beginTransaction().hide(settingsFragment).commit();
             }
         });
 
-        
+        searchTab.setOnClickListener(view -> {
+            @Nullable Fragment searchFragment = getSupportFragmentManager()
+                    .findFragmentByTag(TAG_SEARCH_FRAGMENT);
+
+            if (searchFragment == null) {
+                searchFragment = new SearchFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragment_container, searchFragment, TAG_SEARCH_FRAGMENT)
+                        .commit();
+            }
+            else {
+                getSupportFragmentManager().beginTransaction().show(searchFragment).commit();
+            }
+
+            final @Nullable Fragment featuredFragment = getSupportFragmentManager()
+                    .findFragmentByTag(TAG_FEATURED_FRAGMENT);
+            final @Nullable Fragment settingsFragment = getSupportFragmentManager()
+                    .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
+
+            if (featuredFragment != null) {
+                getSupportFragmentManager().beginTransaction().hide(featuredFragment).commit();
+            }
+            if (settingsFragment != null) {
+                getSupportFragmentManager().beginTransaction().hide(settingsFragment).commit();
+            }
+        });
     }
 }
