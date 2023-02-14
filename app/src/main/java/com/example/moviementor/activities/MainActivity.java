b87b2f3e9c7c9918c7e222749617861102f51e4c
gns12 <garrettspears12@gmail.com>
2023-02-14 15:41:46 -0500

    Link home page to featured tab button in the navigation footer.

--- app/src/main/java/com/example/moviementor/activities/MainActivity.java
+++ app/src/main/java/com/example/moviementor/activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.moviementor.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.moviementor.R;
 import com.example.moviementor.fragments.FeaturedFragment;
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        setupNavigationFooter();
+
         // Fragment is only loaded into container when savedInstanceState is null. This ensures that
         // the home page fragment is added only once: when the main app activity is first created
         if (savedInstanceState == null) {
@@ -22,6 +26,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadFragment(final Class fragmentClass) {
         getSupportFragmentManager().beginTransaction().setReorderingAllowed(true)
-                .add(R.id.fragment_container, fragmentClass, null).commit();
+                .replace(R.id.fragment_container, fragmentClass, null).commit();
+    }
+
+    private void setupNavigationFooter() {
+        // Get the three navigation footer tabs
+        final LinearLayout featuredTab = findViewById(R.id.featured_tab);
+        final LinearLayout searchTab = findViewById(R.id.search_tab);
+        final LinearLayout settingsTab = findViewById(R.id.settings_tab);
+
+        featuredTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //loadFragment(FeaturedFragment.class);
+            }
+        });
+
+        
     }
 }
