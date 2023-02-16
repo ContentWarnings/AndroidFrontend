f8bd898bce335e411fd6a6bc4263458f2231e7bf
gns12 <garrettspears12@gmail.com>
2023-02-15 20:32:16 -0500

    Enabled efficient page switching between the three tabs. Also, highlighted button of whatever tab is currently selected.

--- app/src/main/java/com/example/moviementor/activities/MainActivity.java
+++ app/src/main/java/com/example/moviementor/activities/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private final static String TAG_SETTINGS_FRAGMENT = "SETTINGS_FRAGMENT";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
@@ -27,37 +27,45 @@ public class MainActivity extends AppCompatActivity {
         // Fragment is only loaded into container when savedInstanceState is null. This ensures that
         // the home page fragment is added only once: when the main app activity is first created
         if (savedInstanceState == null) {
-            final Fragment fragment = new FeaturedFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, fragment, TAG_FEATURED_FRAGMENT).commit();
+            // Simulate click on featured tab button in order to make app open to featured page
+            // when app is first launched
+            final LinearLayout featuredTabButton = findViewById(R.id.featured_tab);
+            featuredTabButton.performClick();
         }
     }
 
     private void setupNavigationFooter() {
-        // Get the three navigation footer tabs
+        // Get the three navigation footer tab buttons
         final LinearLayout featuredTab = findViewById(R.id.featured_tab);
         final LinearLayout searchTab = findViewById(R.id.search_tab);
         final LinearLayout settingsTab = findViewById(R.id.settings_tab);
 
+        // Setup click action for featured button
         featuredTab.setOnClickListener(view -> {
+            // Try to get featured page fragment in view hierarchy
             @Nullable Fragment featuredFragment = getSupportFragmentManager()
                     .findFragmentByTag(TAG_FEATURED_FRAGMENT);
 
+            // If featured fragment not found, need to create this fragment and add it to
+            // the view hierarchy
             if (featuredFragment == null) {
                 featuredFragment = new FeaturedFragment();
                 getSupportFragmentManager().beginTransaction()
                         .add(R.id.fragment_container, featuredFragment, TAG_FEATURED_FRAGMENT)
                         .commit();
             }
-            else {
-                getSupportFragmentManager().beginTransaction().show(featuredFragment).commit();
-            }
 
+            // Featured fragment is in view hierarchy, but it may be hidden, so make it visible
+            getSupportFragmentManager().beginTransaction().show(featuredFragment).commit();
+
+            // Try to get the other two page fragments
             final @Nullable Fragment searchFragment = getSupportFragmentManager()
                     .findFragmentByTag(TAG_SEARCH_FRAGMENT);
             final @Nullable Fragment settingsFragment = getSupportFragmentManager()
                     .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
 
+            // If either of the other two page fragments are in the view hierarchy, make sure to
+            // hide them, since the featured fragment should only be shown right now
             if (searchFragment != null) {
                 getSupportFragmentManager().beginTransaction().hide(searchFragment).commit();
             }
@@ -66,25 +74,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Setup click action for search button
         searchTab.setOnClickListener(view -> {
+            // Try to get search page fragment in view hierarchy
             @Nullable Fragment searchFragment = getSupportFragmentManager()
                     .findFragmentByTag(TAG_SEARCH_FRAGMENT);
 
+            // If search fragment not found, need to create this fragment and add it to
+            // the view hierarchy
             if (searchFragment == null) {
                 searchFragment = new SearchFragment();
                 getSupportFragmentManager().beginTransaction()
                         .add(R.id.fragment_container, searchFragment, TAG_SEARCH_FRAGMENT)
                         .commit();
             }
-            else {
-                getSupportFragmentManager().beginTransaction().show(searchFragment).commit();
-            }
 
+            // Search fragment is in view hierarchy, but it may be hidden, so make it visible
+            getSupportFragmentManager().beginTransaction().show(searchFragment).commit();
+
+            // Try to get the other two page fragments
             final @Nullable Fragment featuredFragment = getSupportFragmentManager()
                     .findFragmentByTag(TAG_FEATURED_FRAGMENT);
             final @Nullable Fragment settingsFragment = getSupportFragmentManager()
                     .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
 
+            // If either of the other two page fragments are in the view hierarchy, make sure to
+            // hide them, since the search fragment should only be shown right now
             if (featuredFragment != null) {
                 getSupportFragmentManager().beginTransaction().hide(featuredFragment).commit();
             }
@@ -93,25 +108,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Setup click action for settings button
         settingsTab.setOnClickListener(view -> {
+            // Try to get settings page fragment in view hierarchy
             @Nullable Fragment settingsFragment = getSupportFragmentManager()
                     .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
 
+            // If settings fragment not found, need to create this fragment and add it to
+            // the view hierarchy
             if (settingsFragment == null) {
                 settingsFragment = new SettingsFragment();
                 getSupportFragmentManager().beginTransaction()
                         .add(R.id.fragment_container, settingsFragment, TAG_SETTINGS_FRAGMENT)
                         .commit();
             }
-            else {
-                getSupportFragmentManager().beginTransaction().show(settingsFragment).commit();
-            }
 
+            // Settings fragment is in view hierarchy, but it may be hidden, so make it visible
+            getSupportFragmentManager().beginTransaction().show(settingsFragment).commit();
+
+            // Try to get the other two page fragments
             final @Nullable Fragment featuredFragment = getSupportFragmentManager()
                     .findFragmentByTag(TAG_FEATURED_FRAGMENT);
             final @Nullable Fragment searchFragment = getSupportFragmentManager()
                     .findFragmentByTag(TAG_SEARCH_FRAGMENT);
 
+            // If either of the other two page fragments are in the view hierarchy, make sure to
+            // hide them, since the settings fragment should only be shown right now
             if (featuredFragment != null) {
                 getSupportFragmentManager().beginTransaction().hide(featuredFragment).commit();
             }
--- app/src/main/java/com/example/moviementor/fragments/SearchFragment.java
+++ app/src/main/java/com/example/moviementor/fragments/SearchFragment.java
@@ -1,23 +1,84 @@
 package com.example.moviementor.fragments;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ProgressBar;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.moviementor.R;
 
 public class SearchFragment extends Fragment {
+    private final static String IS_HIDDEN_KEY = "IS_HIDDEN";
+    private boolean isHidden = true;
+
     public SearchFragment() {
         super(R.layout.search_fragment);
     }
 
     @Override
-    public void onViewCreated(final @NonNull View view, final @NonNull Bundle savedInstanceState) {
-        // Bring progress bar to front so it's visible until RecyclerView is populated
-        final ProgressBar loadingProgressWheel = requireActivity().findViewById(R.id.loading_circle);
-        loadingProgressWheel.bringToFront();
+    public void onViewCreated(final @NonNull View view, final @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // If this featured fragment is being created for the first time or it was visible right
+        // before a configuration change (such as device rotation), make this fragment visible
+        if (savedInstanceState == null || !savedInstanceState.getBoolean(IS_HIDDEN_KEY)) {
+            this.onHiddenChanged(false);
+        }
+        // Otherwise, make this fragment hidden
+        else {
+            this.onHiddenChanged(true);
+        }
+
+        // TODO: Setup Search Page
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+
+        // If fragment should be hidden, update hidden boolean to true and make sure that tne
+        // footer button for this fragment is deselected
+        if (hidden) {
+            this.isHidden = true;
+            this.updateSearchTabButton(false);
+        }
+        // Otherwise, update hidden boolean to false and make sure that the footer button for this
+        // fragment is selected
+        else {
+            this.isHidden = false;
+            this.updateSearchTabButton(true);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(final Bundle outState) {
+        // Save current hidden/shown boolean status of fragment in outState, so that when the
+        // device configuration changes (such as on rotation) the fragment's visibility status
+        // can be recovered
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(IS_HIDDEN_KEY, isHidden);
+    }
+
+    private void updateSearchTabButton(final boolean searchTabSelected) {
+        // Get footer icon for this fragment and its background
+        final ImageView searchFooterIcon = requireActivity().findViewById(R.id.search_icon);
+        final GradientDrawable gradientDrawable = (GradientDrawable) searchFooterIcon.getBackground();
+
+        // If this footer icon is selected, change its background color to purple
+        if (searchTabSelected) {
+            gradientDrawable.setColor(getResources().
+                    getColor(R.color.nav_button_selected_background_color, null));
+        }
+        // Otherwise, make its background color transparent
+        else {
+            gradientDrawable.setColor(getResources().
+                    getColor(R.color.nav_button_unselected_background_color, null));
+        }
+
+        searchFooterIcon.setBackground(gradientDrawable);
     }
 }
