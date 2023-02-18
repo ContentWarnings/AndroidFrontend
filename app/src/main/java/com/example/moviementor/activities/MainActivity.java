1c0d9da511de6f31e0794d1c07b2723ed9e6568d
gns12 <garrettspears12@gmail.com>
2023-02-17 20:10:32 -0500

    Got fragment stacks working and being preserved after device configuration change.

--- app/src/main/java/com/example/moviementor/activities/MainActivity.java
+++ app/src/main/java/com/example/moviementor/activities/MainActivity.java
@@ -1,26 +1,40 @@
 package com.example.moviementor.activities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 
 import com.example.moviementor.R;
+import com.example.moviementor.fragments.BaseFragment;
 import com.example.moviementor.fragments.FeaturedFragment;
 import com.example.moviementor.fragments.SearchFragment;
 import com.example.moviementor.fragments.SettingsFragment;
+import com.example.moviementor.other.FragmentStackManager;
 
 public class MainActivity extends AppCompatActivity {
     public final static String TAG_FEATURED_FRAGMENT = "FEATURED_FRAGMENT";
     public final static String TAG_SEARCH_FRAGMENT = "SEARCH_FRAGMENT";
     public final static String TAG_SETTINGS_FRAGMENT = "SETTINGS_FRAGMENT";
 
+    @NonNull FragmentStackManager fragmentStackManager;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        this.fragmentStackManager = FragmentStackManager.getInstance();
+        Log.d("YOO: ", "stack sizes: " +  fragmentStackManager.featuredTabStack.size() +
+        " " + fragmentStackManager.searchTabStack.size() + " " + fragmentStackManager.settingsTabStack.size());
+
+        if (savedInstanceState != null) {
+            fragmentStackManager.replaceTopLevelFragmentsOnStacks(getSupportFragmentManager());
+        }
 
         setupNavigationFooter();
 
@@ -34,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+//    @Override
+//    public void onSaveInstanceState(final Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//
+//        // Save the fragment stack
+//        outState.put
+//        outState.putParcelableArrayList("fragmentStack", new ArrayList<>(mFragmentStack));
+//    }
+
     private void setupNavigationFooter() {
         // Get the three navigation footer tab buttons
         final LinearLayout featuredTab = findViewById(R.id.featured_tab);
@@ -42,104 +65,109 @@ public class MainActivity extends AppCompatActivity {
 
         // Setup click action for featured button
         featuredTab.setOnClickListener(view -> {
-            // Try to get featured page fragment in view hierarchy
-            @Nullable Fragment featuredFragment = getSupportFragmentManager()
-                    .findFragmentByTag(TAG_FEATURED_FRAGMENT);
-
-            // If featured fragment not found, need to create this fragment and add it to
-            // the view hierarchy
-            if (featuredFragment == null) {
-                featuredFragment = new FeaturedFragment();
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.fragment_container, featuredFragment, TAG_FEATURED_FRAGMENT)
-                        .commit();
-            }
-
-            // Featured fragment is in view hierarchy, but it may be hidden, so make it visible
-            getSupportFragmentManager().beginTransaction().show(featuredFragment).commit();
-
-            // Try to get the other two page fragments
-            final @Nullable Fragment searchFragment = getSupportFragmentManager()
-                    .findFragmentByTag(TAG_SEARCH_FRAGMENT);
-            final @Nullable Fragment settingsFragment = getSupportFragmentManager()
-                    .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
-
-            // If either of the other two page fragments are in the view hierarchy, make sure to
-            // hide them, since the featured fragment should only be shown right now
-            if (searchFragment != null) {
-                getSupportFragmentManager().beginTransaction().hide(searchFragment).commit();
-            }
-            if (settingsFragment != null) {
-                getSupportFragmentManager().beginTransaction().hide(settingsFragment).commit();
-            }
+            fragmentStackManager.switchTab(getSupportFragmentManager(), BaseFragment.Tab.FEATURED);
+//            // Try to get featured page fragment in view hierarchy
+//            @Nullable Fragment featuredFragment = getSupportFragmentManager()
+//                    .findFragmentByTag(TAG_FEATURED_FRAGMENT);
+//
+//            // If featured fragment not found, need to create this fragment and add it to
+//            // the view hierarchy
+//            if (featuredFragment == null) {
+//                featuredFragment = new FeaturedFragment();
+//                getSupportFragmentManager().beginTransaction()
+//                        .add(R.id.fragment_container, featuredFragment, TAG_FEATURED_FRAGMENT)
+//                        .commit();
+//            }
+//
+//            // Featured fragment is in view hierarchy, but it may be hidden, so make it visible
+//            getSupportFragmentManager().beginTransaction().show(featuredFragment).commit();
+//
+//            // Try to get the other two page fragments
+//            final @Nullable Fragment searchFragment = getSupportFragmentManager()
+//                    .findFragmentByTag(TAG_SEARCH_FRAGMENT);
+//            final @Nullable Fragment settingsFragment = getSupportFragmentManager()
+//                    .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
+//
+//            // If either of the other two page fragments are in the view hierarchy, make sure to
+//            // hide them, since the featured fragment should only be shown right now
+//            if (searchFragment != null) {
+//                getSupportFragmentManager().beginTransaction().hide(searchFragment).commit();
+//            }
+//            if (settingsFragment != null) {
+//                getSupportFragmentManager().beginTransaction().hide(settingsFragment).commit();
+//            }
         });
 
         // Setup click action for search button
         searchTab.setOnClickListener(view -> {
-            // Try to get search page fragment in view hierarchy
-            @Nullable Fragment searchFragment = getSupportFragmentManager()
-                    .findFragmentByTag(TAG_SEARCH_FRAGMENT);
+            fragmentStackManager.switchTab(getSupportFragmentManager(), BaseFragment.Tab.SEARCH);
 
-            // If search fragment not found, need to create this fragment and add it to
-            // the view hierarchy
-            if (searchFragment == null) {
-                searchFragment = new SearchFragment();
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.fragment_container, searchFragment, TAG_SEARCH_FRAGMENT)
-                        .commit();
-            }
-
-            // Search fragment is in view hierarchy, but it may be hidden, so make it visible
-            getSupportFragmentManager().beginTransaction().show(searchFragment).commit();
-
-            // Try to get the other two page fragments
-            final @Nullable Fragment featuredFragment = getSupportFragmentManager()
-                    .findFragmentByTag(TAG_FEATURED_FRAGMENT);
-            final @Nullable Fragment settingsFragment = getSupportFragmentManager()
-                    .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
-
-            // If either of the other two page fragments are in the view hierarchy, make sure to
-            // hide them, since the search fragment should only be shown right now
-            if (featuredFragment != null) {
-                getSupportFragmentManager().beginTransaction().hide(featuredFragment).commit();
-            }
-            if (settingsFragment != null) {
-                getSupportFragmentManager().beginTransaction().hide(settingsFragment).commit();
-            }
+//            // Try to get search page fragment in view hierarchy
+//            @Nullable Fragment searchFragment = getSupportFragmentManager()
+//                    .findFragmentByTag(TAG_SEARCH_FRAGMENT);
+//
+//            // If search fragment not found, need to create this fragment and add it to
+//            // the view hierarchy
+//            if (searchFragment == null) {
+//                searchFragment = new SearchFragment();
+//                getSupportFragmentManager().beginTransaction()
+//                        .add(R.id.fragment_container, searchFragment, TAG_SEARCH_FRAGMENT)
+//                        .commit();
+//            }
+//
+//            // Search fragment is in view hierarchy, but it may be hidden, so make it visible
+//            getSupportFragmentManager().beginTransaction().show(searchFragment).commit();
+//
+//            // Try to get the other two page fragments
+//            final @Nullable Fragment featuredFragment = getSupportFragmentManager()
+//                    .findFragmentByTag(TAG_FEATURED_FRAGMENT);
+//            final @Nullable Fragment settingsFragment = getSupportFragmentManager()
+//                    .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
+//
+//            // If either of the other two page fragments are in the view hierarchy, make sure to
+//            // hide them, since the search fragment should only be shown right now
+//            if (featuredFragment != null) {
+//                getSupportFragmentManager().beginTransaction().hide(featuredFragment).commit();
+//            }
+//            if (settingsFragment != null) {
+//                getSupportFragmentManager().beginTransaction().hide(settingsFragment).commit();
+//            }
         });
 
         // Setup click action for settings button
         settingsTab.setOnClickListener(view -> {
-            // Try to get settings page fragment in view hierarchy
-            @Nullable Fragment settingsFragment = getSupportFragmentManager()
-                    .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
+            fragmentStackManager.switchTab(getSupportFragmentManager(), BaseFragment.Tab.SETTINGS);
 
-            // If settings fragment not found, need to create this fragment and add it to
-            // the view hierarchy
-            if (settingsFragment == null) {
-                settingsFragment = new SettingsFragment();
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.fragment_container, settingsFragment, TAG_SETTINGS_FRAGMENT)
-                        .commit();
-            }
-
-            // Settings fragment is in view hierarchy, but it may be hidden, so make it visible
-            getSupportFragmentManager().beginTransaction().show(settingsFragment).commit();
-
-            // Try to get the other two page fragments
-            final @Nullable Fragment featuredFragment = getSupportFragmentManager()
-                    .findFragmentByTag(TAG_FEATURED_FRAGMENT);
-            final @Nullable Fragment searchFragment = getSupportFragmentManager()
-                    .findFragmentByTag(TAG_SEARCH_FRAGMENT);
-
-            // If either of the other two page fragments are in the view hierarchy, make sure to
-            // hide them, since the settings fragment should only be shown right now
-            if (featuredFragment != null) {
-                getSupportFragmentManager().beginTransaction().hide(featuredFragment).commit();
-            }
-            if (searchFragment != null) {
-                getSupportFragmentManager().beginTransaction().hide(searchFragment).commit();
-            }
+//            // Try to get settings page fragment in view hierarchy
+//            @Nullable Fragment settingsFragment = getSupportFragmentManager()
+//                    .findFragmentByTag(TAG_SETTINGS_FRAGMENT);
+//
+//            // If settings fragment not found, need to create this fragment and add it to
+//            // the view hierarchy
+//            if (settingsFragment == null) {
+//                settingsFragment = new SettingsFragment();
+//                getSupportFragmentManager().beginTransaction()
+//                        .add(R.id.fragment_container, settingsFragment, TAG_SETTINGS_FRAGMENT)
+//                        .commit();
+//            }
+//
+//            // Settings fragment is in view hierarchy, but it may be hidden, so make it visible
+//            getSupportFragmentManager().beginTransaction().show(settingsFragment).commit();
+//
+//            // Try to get the other two page fragments
+//            final @Nullable Fragment featuredFragment = getSupportFragmentManager()
+//                    .findFragmentByTag(TAG_FEATURED_FRAGMENT);
+//            final @Nullable Fragment searchFragment = getSupportFragmentManager()
+//                    .findFragmentByTag(TAG_SEARCH_FRAGMENT);
+//
+//            // If either of the other two page fragments are in the view hierarchy, make sure to
+//            // hide them, since the settings fragment should only be shown right now
+//            if (featuredFragment != null) {
+//                getSupportFragmentManager().beginTransaction().hide(featuredFragment).commit();
+//            }
+//            if (searchFragment != null) {
+//                getSupportFragmentManager().beginTransaction().hide(searchFragment).commit();
+//            }
         });
     }
 }
--- app/src/main/java/com/example/moviementor/other/FragmentStackManager.java
+++ app/src/main/java/com/example/moviementor/other/FragmentStackManager.java
@@ -1,18 +1,25 @@
 package com.example.moviementor.other;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.moviementor.R;
 import com.example.moviementor.activities.MainActivity;
 import com.example.moviementor.fragments.BaseFragment;
 import com.example.moviementor.fragments.BaseFragment.Tab;
+import com.example.moviementor.fragments.FeaturedFragment;
+import com.example.moviementor.fragments.SearchFragment;
+import com.example.moviementor.fragments.SettingsFragment;
 
 public class FragmentStackManager {
     private static FragmentStackManager instance;
 
-    private final FragmentStack featuredTabStack;
-    private final FragmentStack searchTabStack;
-    private final FragmentStack settingsTabStack;
+    public final FragmentStack featuredTabStack;
+    public final FragmentStack searchTabStack;
+    public final FragmentStack settingsTabStack;
 
     private FragmentStackManager () {
         // Init fragment stacks for each tab
@@ -29,7 +36,88 @@ public class FragmentStackManager {
         return instance;
     }
 
-    public void openNewPage(final @NonNull MainActivity mainActivity,
+    private BaseFragment getStartFragment(final @NonNull Tab tab) {
+        if (tab == Tab.FEATURED) {
+            return new FeaturedFragment();
+        }
+        else if (tab == Tab.SEARCH) {
+            return new SearchFragment();
+        }
+        else {
+            return new SettingsFragment();
+        }
+    }
+
+    public void replaceTopLevelFragmentsOnStacks(final @NonNull FragmentManager fragmentManager) {
+        final @Nullable BaseFragment currentFeaturedFragment = (BaseFragment) fragmentManager.
+                findFragmentByTag(MainActivity.TAG_FEATURED_FRAGMENT);
+        final @Nullable BaseFragment currentSearchFragment = (BaseFragment) fragmentManager.
+                findFragmentByTag(MainActivity.TAG_SEARCH_FRAGMENT);
+        final @Nullable BaseFragment currentSettingsFragment = (BaseFragment) fragmentManager.
+                findFragmentByTag(MainActivity.TAG_SETTINGS_FRAGMENT);
+
+        if (currentFeaturedFragment != null) {
+            this.featuredTabStack.pop();
+            this.featuredTabStack.push(currentFeaturedFragment);
+        }
+        if (currentSearchFragment != null) {
+            this.searchTabStack.pop();
+            this.searchTabStack.push(currentSearchFragment);
+        }
+        if (currentSettingsFragment != null) {
+            this.settingsTabStack.pop();
+            this.settingsTabStack.push(currentSettingsFragment);
+        }
+    }
+
+    public void switchTab(final @NonNull FragmentManager fragmentManager,
+                          final @NonNull Tab newTab) {
+        FragmentStack destFragmentStack;
+        String tag;
+
+        if (newTab == Tab.FEATURED) {
+            destFragmentStack = this.featuredTabStack;
+            tag = MainActivity.TAG_FEATURED_FRAGMENT;
+        }
+        else if (newTab == Tab.SEARCH) {
+            destFragmentStack = this.searchTabStack;
+            tag = MainActivity.TAG_SEARCH_FRAGMENT;
+        }
+        else {
+            destFragmentStack = this.settingsTabStack;
+            tag = MainActivity.TAG_SETTINGS_FRAGMENT;
+        }
+
+        final BaseFragment topFragment;
+
+        if (destFragmentStack.isEmpty()) {
+            topFragment = getStartFragment(newTab);
+            destFragmentStack.push(topFragment);
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, topFragment, tag)
+                    .commit();
+        }
+        else {
+            topFragment = destFragmentStack.getTopFragment();
+        }
+
+        fragmentManager.beginTransaction().show(topFragment).commit();
+
+        if (newTab != Tab.FEATURED && !featuredTabStack.isEmpty()) {
+            final BaseFragment featuredFragment = featuredTabStack.getTopFragment();
+            fragmentManager.beginTransaction().hide(featuredFragment).commit();
+        }
+        if (newTab != Tab.SEARCH && !searchTabStack.isEmpty()) {
+            final BaseFragment searchFragment = searchTabStack.getTopFragment();
+            fragmentManager.beginTransaction().hide(searchFragment).commit();
+        }
+        if (newTab != Tab.SETTINGS && !settingsTabStack.isEmpty()) {
+            final BaseFragment settingsFragment = settingsTabStack.getTopFragment();
+            fragmentManager.beginTransaction().hide(settingsFragment).commit();
+        }
+    }
+
+    public void openNewPage(final @NonNull FragmentManager fragmentManager,
                             final @NonNull BaseFragment fragment,
                             final @NonNull Tab currentTab) {
         String tag;
@@ -47,11 +135,10 @@ public class FragmentStackManager {
             tag = MainActivity.TAG_SETTINGS_FRAGMENT;
         }
 
-        mainActivity.getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, fragment, tag).commit();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, tag).commit();
     }
 
-    public void goBackAPage(final @NonNull MainActivity mainActivity,
+    public void goBackAPage(final @NonNull FragmentManager fragmentManager,
                             final @NonNull Tab currentTab) {
         FragmentStack currFragmentStack;
         String tag;
@@ -59,12 +146,10 @@ public class FragmentStackManager {
         if (currentTab == Tab.FEATURED) {
             currFragmentStack = this.featuredTabStack;
             tag = MainActivity.TAG_FEATURED_FRAGMENT;
-        }
-        else if (currentTab == Tab.SEARCH) {
+        } else if (currentTab == Tab.SEARCH) {
             currFragmentStack = this.searchTabStack;
             tag = MainActivity.TAG_SEARCH_FRAGMENT;
-        }
-        else {
+        } else {
             currFragmentStack = this.settingsTabStack;
             tag = MainActivity.TAG_SETTINGS_FRAGMENT;
         }
@@ -76,7 +161,7 @@ public class FragmentStackManager {
         currFragmentStack.pop();
         final BaseFragment newTopFragment = currFragmentStack.getTopFragment();
 
-        mainActivity.getSupportFragmentManager().beginTransaction()
+        fragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, newTopFragment, tag).commit();
     }
 }
