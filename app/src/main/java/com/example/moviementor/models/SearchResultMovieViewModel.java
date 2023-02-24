262ec6d8668b4dbaba2af7b3ec525af8b6c88d12
gns12 <garrettspears12@gmail.com>
2023-02-24 15:13:06 -0500

    Created view model class to store each movie from search results.

---
+++ app/src/main/java/com/example/moviementor/models/SearchResultMovieViewModel.java
@@ -0,0 +1,31 @@
+package com.example.moviementor.models;
+
+import androidx.annotation.NonNull;
+
+import java.util.Date;
+
+public class SearchResultMovieViewModel {
+    private final int movieId;
+    private final @NonNull String movieName;
+    private final @NonNull Date releaseDate;
+    private final @NonNull String movieImageUrl;
+    private final @NonNull String movieOverview;
+    private final int runtime;
+    private final String[] genres;
+    private final String[] contentWarnings;
+
+    public SearchResultMovieViewModel(final int movieId, final @NonNull String movieName,
+                                      final @NonNull Date releaseDate, final @NonNull String movieImageUrl,
+                                      final @NonNull String movieOverview, final int runtime,
+                                      final String[] genres, final String[] contentWarnings) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.releaseDate = releaseDate;
+        this.movieImageUrl = movieImageUrl;
+        this.movieOverview = movieOverview;
+        this.runtime = runtime;
+        this.genres = genres;
+        this.contentWarnings = contentWarnings;
+    }
+
+}
