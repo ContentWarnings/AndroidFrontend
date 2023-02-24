588e8d070572d93894094da95255abbb3cfbd3bb
gns12 <garrettspears12@gmail.com>
2023-02-24 16:38:52 -0500

    Parsed search results from API into view model objects.

--- app/src/main/java/com/example/moviementor/other/Backend.java
+++ app/src/main/java/com/example/moviementor/other/Backend.java
@@ -3,9 +3,11 @@ package com.example.moviementor.other;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.moviementor.adapters.SearchPageAdapter;
 import com.example.moviementor.fragments.FeaturedFragment;
+import com.example.moviementor.models.SearchResultMovieViewModel;
 import com.example.moviementor.models.TrendingMovieViewModel;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -14,13 +16,21 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import cz.msebera.android.httpclient.Header;
 
 public class Backend {
     private static final String SEARCH_TAG = "SEARCH";
+    private static final int INVALID_MOVIE_ID = -1;
+    private static final String DATE_FORMAT_STRING = "dd-mm-yyyy";
 
     // Base url that will be used for all API requests
     private static final String baseUrl = "https://api.moviementor.app/";
@@ -62,8 +72,13 @@ public class Backend {
                     for (int i = 0; i < searchResults.length(); i++) {
                         final JSONObject searchResult = searchResults.getJSONObject(i);
 
-                        final @NonNull String movieTitle = searchResult.getString("title");
-                        final @NonNull String movieImageUrl = searchResult.getString("img");
+                        // Skip over any invalid results
+                        if (searchResult == null) {
+                            continue;
+                        }
+
+                        final @NonNull String movieTitle = searchResult.optString("title", "");
+                        final @NonNull String movieImageUrl = searchResult.optString("img", "");
 
                         final TrendingMovieViewModel movieData = new TrendingMovieViewModel(movieTitle, movieImageUrl);
                         trendingMoviesList.add(movieData);
@@ -97,12 +112,91 @@ public class Backend {
         client.get(getAbsoluteUrl(searchParamsUrl), new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                final @NonNull List<SearchResultMovieViewModel> searchResultViewModels = new ArrayList<>();
+
                 try {
                     final JSONArray searchResults = new JSONObject(new String(responseBody)).getJSONArray("results");
 
-                    // Decode each movie's data into a SearchResultViewModel object
+                    // Used to parse through date strings returned from the backend
+                    final SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT_STRING, Locale.US);
+
+                    // Decode each movie's data into a SearchResultMovieViewModel object
                     for (int i = 0; i < searchResults.length(); i++) {
                         final JSONObject searchResult = searchResults.getJSONObject(i);
+
+                        // Skip over any invalid search results
+                        if (searchResult == null) {
+                            continue;
+                        }
+
+                        final int movieId = searchResult.optInt("id", INVALID_MOVIE_ID);
+
+                        // Can not find movie's id, so move onto next result
+                        if (movieId == INVALID_MOVIE_ID) {
+                            continue;
+                        }
+
+                        final @NonNull String movieName = searchResult.optString("title", "");
+                        final @NonNull String releaseDateString = searchResult.optString("release", "");
+                        final @NonNull String movieImageUrlString = searchResult.optString("img", "");
+                        final @NonNull String movieOverview = searchResult.optString("overview", "");
+                        final int runtime = searchResult.optInt("runtime", SearchResultMovieViewModel.MISSING_RUNTIME);
+                        final @Nullable JSONArray genreJSONArray = searchResult.optJSONArray("genres");
+                        final @Nullable JSONArray contentWarningsJSONArray = searchResult.optJSONArray("cw");
+
+                        // Try parsing date string and default to null if unsuccessful
+                        @Nullable Date releaseDate;
+                        try {
+                            releaseDate = formatter.parse(releaseDateString);
+                        }
+                        catch (final ParseException e) {
+                            releaseDate = null;
+                        }
+
+                        // Try making URL object and default to null if unsuccessful
+                        @Nullable URL movieImageUrl;
+                        try {
+                            movieImageUrl = new URL(movieImageUrlString);
+                        }
+                        catch(final MalformedURLException e) {
+                            movieImageUrl = null;
+                        }
+
+                        // Parse through genre strings and add valid genres to the list
+                        final @NonNull List<String> genreList = new ArrayList<>();
+                        if (genreJSONArray != null) {
+                            for (int j = 0; j < genreJSONArray.length(); j++) {
+                                final @Nullable String genre = genreJSONArray.optString(j, null);
+
+                                // Skip over any invalid strings in the genres array
+                                if (genre == null) {
+                                    continue;
+                                }
+
+                                genreList.add(genre);
+                            }
+                        }
+
+                        // Parse through content warning strings and add valid content warnings to the list
+                        final @NonNull List<String> contentWarningList = new ArrayList<>();
+                        if (contentWarningsJSONArray != null) {
+                            for (int j = 0; j < contentWarningsJSONArray.length(); j++) {
+                                final @Nullable String contentWarning = contentWarningsJSONArray.optString(j, null);
+
+                                // Skip over any invalid strings in the content warnings array
+                                if (contentWarning == null) {
+                                    continue;
+                                }
+
+                                contentWarningList.add(contentWarning);
+                            }
+                        }
+
+                        final @NonNull SearchResultMovieViewModel searchResultViewModel =
+                                new SearchResultMovieViewModel(movieId, movieName, releaseDate,
+                                        movieImageUrl, movieOverview, runtime, genreList,
+                                        contentWarningList);
+                        searchResultViewModels.add(searchResultViewModel);
                     }
                 }
                 catch (final JSONException e) {
