6df6f12713cca6185c52e9551d54b690d16f79f2
gns12 <garrettspears12@gmail.com>
2023-02-28 20:32:18 -0500

    Set up basic layout of advanced search options modal and set up layout of filter by buttons.

--- app/src/main/java/com/example/moviementor/other/AdvancedSearchOptionsModal.java
+++ app/src/main/java/com/example/moviementor/other/AdvancedSearchOptionsModal.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 
 import com.example.moviementor.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class AdvancedSearchOptionsModal extends BottomSheetDialogFragment {
@@ -18,5 +19,14 @@ public class AdvancedSearchOptionsModal extends BottomSheetDialogFragment {
         final View advancedSearchOptionsModal = inflater.inflate(R.layout.advanced_search_options_modal,
                 container, false);
         return advancedSearchOptionsModal;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Bug fix to make dialog fragment open fully in landscape mode
+        final BottomSheetBehavior<View> view = BottomSheetBehavior.from((View) requireView().getParent());
+        view.setState(BottomSheetBehavior.STATE_EXPANDED);
+        view.setMaxWidth(ViewGroup.LayoutParams.MATCH_PARENT);
     }
 }
