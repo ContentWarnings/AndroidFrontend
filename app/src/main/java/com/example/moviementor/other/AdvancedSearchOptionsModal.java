4b24a39e20edfce51d82d1acc1508b7c514c8645
gns12 <garrettspears12@gmail.com>
2023-02-28 20:49:54 -0500

    Got close button working for advanced search options modal.

--- app/src/main/java/com/example/moviementor/other/AdvancedSearchOptionsModal.java
+++ app/src/main/java/com/example/moviementor/other/AdvancedSearchOptionsModal.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 
@@ -18,12 +19,21 @@ public class AdvancedSearchOptionsModal extends BottomSheetDialogFragment {
     {
         final View advancedSearchOptionsModal = inflater.inflate(R.layout.advanced_search_options_modal,
                 container, false);
+
+        final ImageButton closeModalButton = advancedSearchOptionsModal
+                .findViewById(R.id.close_advanced_options_menu_button);
+
+        // When "x" button is pressed in top left corner of modal, then close
+        // the modal
+        closeModalButton.setOnClickListener(view -> dismiss());
+
         return advancedSearchOptionsModal;
     }
 
     @Override
     public void onStart() {
         super.onStart();
+
         // Bug fix to make dialog fragment open fully in landscape mode
         final BottomSheetBehavior<View> view = BottomSheetBehavior.from((View) requireView().getParent());
         view.setState(BottomSheetBehavior.STATE_EXPANDED);
