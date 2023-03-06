c092c26d2b364efac649980639d03027d0ef60a3
gns12 <garrettspears12@gmail.com>
2023-03-06 15:31:07 -0500

    Refactored trending movie view model class to hold onto movie ids too and fixed parsing of trending movie data to be safer.

--- app/src/main/java/com/example/moviementor/models/TrendingMovieViewModel.java
+++ app/src/main/java/com/example/moviementor/models/TrendingMovieViewModel.java
@@ -1,14 +1,24 @@
 package com.example.moviementor.models;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.net.URL;
 
 public class TrendingMovieViewModel {
+    private final int movieId;
     private final @NonNull String movieName;
-    private final @NonNull String movieImageUrl;
+    private final @NonNull URL movieImageUrl;
 
-    public TrendingMovieViewModel(final @NonNull String movieName, final @NonNull String movieImageUrl) {
+    public TrendingMovieViewModel(final int movieId, final @NonNull String movieName,
+                                  final @NonNull URL movieImageUrl) {
+        this.movieId = movieId;
         this.movieName = movieName;
         this.movieImageUrl = movieImageUrl;
+    }
+
+    public int getMovieId() {
+        return this.movieId;
     }
 
     @NonNull
@@ -16,8 +26,8 @@ public class TrendingMovieViewModel {
         return this.movieName;
     }
 
-    @NonNull
-    public String getMovieImageUrl() {
+    @Nullable
+    public URL getMovieImageUrl() {
         return this.movieImageUrl;
     }
 }
