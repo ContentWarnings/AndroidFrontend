3129d0385e26c124052b245fedf363a5d1be21d4
gns12 <garrettspears12@gmail.com>
2023-03-16 21:28:53 -0400

    Setup header on settings page.

--- app/src/main/java/com/example/moviementor/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/moviementor/fragments/SettingsFragment.java
@@ -2,11 +2,14 @@ package com.example.moviementor.fragments;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.moviementor.R;
+import com.example.moviementor.activities.MainActivity;
 
 public class SettingsFragment extends BaseFragment {
 
@@ -17,6 +20,22 @@ public class SettingsFragment extends BaseFragment {
     @Override
     public void onViewCreated(final @NonNull View view, final @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // TODO: Setup Settings Page
+
+        setupSettingsPage();
+    }
+
+    private void setupSettingsPage() {
+        final TextView headerTitle = requireView().findViewById(R.id.header_title);
+        final ImageButton headerSearchButton = requireView().findViewById(R.id.header_search_button);
+
+        // Set the setting page's header with the corresponding title text
+        headerTitle.setText(R.string.settings_page_header_title);
+
+        // Setup click listener on header's search button to start user's request of jumping to the
+        // search bar on the search page
+        headerSearchButton.setOnClickListener(v -> {
+            final MainActivity mainActivity = (MainActivity) requireActivity();
+            mainActivity.jumpToSearchBar();
+        });
     }
 }
